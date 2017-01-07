8a4b0c0a0b524e693e8fe7b19279b3ad878ddcbe
Deyvison Guilherme <deyvison.guilherme@live.com>
2017-01-07 14:44:14 -0200

    loaded phoenix framework

---
+++ lib/senem/repo.ex
@@ -0,0 +1,3 @@
+defmodule Senem.Repo do
+  use Ecto.Repo, otp_app: :senem
+end
---
+++ test/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule Senem.PageControllerTest do
+  use Senem.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
